c0b2a93c6790b200c35eb810be763a6e65a082e6
das11 <kabird1196@gmail.com>
2016-09-11 13:25:47 +0530

    GPS notif

--- app/src/main/java/kdas/i_nterface/uitest_2/Home.java
+++ app/src/main/java/kdas/i_nterface/uitest_2/Home.java
@@ -2,8 +2,13 @@ package kdas.i_nterface.uitest_2;
 
 import android.app.Notification;
 import android.content.Intent;
+import android.content.IntentSender;
 import android.content.SharedPreferences;
+import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.util.Log;
@@ -16,6 +21,19 @@ import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -25,7 +43,9 @@ import br.com.goncalves.pugnotification.notification.PugNotification;
 import xyz.hanks.library.SmallBang;
 import xyz.hanks.library.SmallBangListener;
 
-public class Home extends AppCompatActivity {
+public class Home extends AppCompatActivity implements LocationListener,
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener{
 
     Thread ref;
     Firebase notif, pinged, pinger;
@@ -41,6 +61,8 @@ public class Home extends AppCompatActivity {
     Animation slide_up;
     SmallBang mSmallBang;
 
+    private GoogleApiClient googleApiClient;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,9 +72,6 @@ public class Home extends AppCompatActivity {
 
         getSupportActionBar().hide();
         mSmallBang = SmallBang.attach2Window(this);
-
-
-
         Firebase.setAndroidContext(this);
 
         //android.support.v7.widget.Toolbar  toolbar = (android.support.v7.widget.Toolbar ) findViewById(R.id.main2toolbar);
@@ -62,34 +81,7 @@ public class Home extends AppCompatActivity {
 //            toolbar.setTitle("SOLO");
 //        toolbar.setTitleTextColor(getResources().getColor(R.color.some_white));
 //        setSupportActionBar(toolbar);
-
-        SharedPreferences pref = getSharedPreferences("prefs", MODE_PRIVATE);
-        user_num = pref.getString("Number","");
-        init = pref.getBoolean("Init", init);
-        Log.d("####", user_num);
-        Log.d("usr", user_num);
-        Log.d("init", init + "");
-
         slide_up = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.popup);
-
-
-
-        if (!init){
-            Intent i = new Intent(Home.this, Init.class);
-            startActivity(i);
-        }
-
-        ScheduledExecutorService scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
-        scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
-            public void run() {
-                if (!thread){
-                    check_notif();
-                    Log.d("Keep", "running");
-                }
-            }
-        }, 0, 10, TimeUnit.SECONDS);
-
-
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_main_1);
 //        if (fab != null){
 //            fab.setOnClickListener(new View.OnClickListener() {
@@ -182,7 +174,6 @@ public class Home extends AppCompatActivity {
 //
 //            }
 //        });
-
         fab2 = (CardView) findViewById(R.id.view10);
         fab2.startAnimation(slide_up);
         fab2.setOnClickListener(new View.OnClickListener() {
@@ -263,7 +254,31 @@ public class Home extends AppCompatActivity {
             }
         });
 
+        new do_stuff().execute("");
 
+
+    }
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {}
+
+    @Override
+    public void onConnectionSuspended(int i) {}
+
+    @Override
+    public void onLocationChanged(Location location) {}
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {}
+
+    private class do_stuff extends AsyncTask<String, Void, String>{
+
+        @Override
+        protected String doInBackground(String... strings) {
+            pref_and_init();
+            gps();
+            return null;
+        }
     }
 
     public void check_notif(){
@@ -355,5 +370,85 @@ public class Home extends AppCompatActivity {
                 .simple()
                 .build();
 
+    }
+
+    public void pref_and_init(){
+        SharedPreferences pref = getSharedPreferences("prefs", MODE_PRIVATE);
+        user_num = pref.getString("Number","");
+        init = pref.getBoolean("Init", init);
+        Log.d("####", user_num);
+        Log.d("usr", user_num);
+        Log.d("init", init + "");
+
+
+
+
+        if (!init){
+            Intent i = new Intent(Home.this, Init.class);
+            startActivity(i);
+        }
+
+        ScheduledExecutorService scheduleTaskExecutor = Executors.newScheduledThreadPool(5);
+        scheduleTaskExecutor.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                if (!thread){
+                    check_notif();
+                    Log.d("Keep", "running");
+                }
+            }
+        }, 0, 10, TimeUnit.SECONDS);
+    }
+
+    public void gps(){
+        googleApiClient = new GoogleApiClient.Builder(getApplicationContext())
+                .addApi(LocationServices.API)
+                .build();
+        googleApiClient.connect();
+
+        LocationRequest locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(30 * 1000);
+        locationRequest.setFastestInterval(5 * 1000);
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest);
+
+        builder.setAlwaysShow(true);
+
+        PendingResult<LocationSettingsResult> result = LocationServices.SettingsApi.checkLocationSettings(googleApiClient, builder.build());
+
+        result.setResultCallback(new ResultCallback<LocationSettingsResult>() {
+            @Override
+            public void onResult(LocationSettingsResult result) {
+                final Status status = result.getStatus();
+                final LocationSettingsStates locationSettingsStates = result.getLocationSettingsStates();
+                switch (status.getStatusCode()) {
+                    case LocationSettingsStatusCodes.SUCCESS:
+                        // All location settings are satisfied. The client can initialize location
+                        // requests here.
+
+                        break;
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied. But could be fixed by showing the user
+                        // a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            status.startResolutionForResult(Home.this,1000);
+                        } catch (IntentSender.SendIntentException e) {
+                            // Ignore the error.
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way to fix the
+                        // settings so we won't show the dialog.
+                        break;
+
+                    case  LocationSettingsStatusCodes.CANCELED:
+                        Toast.makeText(getApplicationContext(), "We don't have a Warp Drive, so turn the GPS ON", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        googleApiClient = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 }
